95fc2f615e7673cb46811c4466a20911c3617ee2
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2020-05-28 15:18:44 +0200

    Memoize sdk class hook

--- src/@sdk/react/useHook.ts
+++ src/@sdk/react/useHook.ts
@@ -1,14 +1,15 @@
+import React from "react";
 import { SaleorAPI } from "../api";
 import { useSaleorClient } from "./helpers";
 
 const useHook = <T extends keyof SaleorAPI>(dataName: T): SaleorAPI[T] => {
   const saleor = useSaleorClient();
 
-  const getHookData = () => {
+  const getHookData = React.useMemo(() => {
     return saleor[dataName];
-  };
+  }, []);
 
-  return getHookData();
+  return getHookData;
 };
 
 export const hookFactory = <T extends keyof SaleorAPI>(query: T) => () =>
